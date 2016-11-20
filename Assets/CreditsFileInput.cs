0f5e1b6ae8e6eb2bd9bd98dfe39df88c288a09a7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-11-19 23:46:59 -0800

    credits

---
+++ Assets/CreditsFileInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CreditsFileInput : MonoBehaviour {
+    public TextAsset credits;
+
+	void Update () {
+        gameObject.GetComponent<Text>().text = credits.text;
+	}
+}
