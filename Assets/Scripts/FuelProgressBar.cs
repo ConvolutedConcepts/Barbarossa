46f986f21f4b226ac3b86b54c1162413bc273762
Yaro <yaro@koder.ai>
2016-11-21 20:47:44 -0800

    Added health bar on level 1 Added script that handles the handle bar

---
+++ Assets/Scripts/FuelProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class FuelProgressBar : MonoBehaviour {
+
+	[SerializeField]
+	private float fillAmount;
+
+	[SerializeField]
+	private Image blackRect;
+	// Use this for initialization
+	void Start () {
+		blackRect.fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleBar ();
+	}
+	private void HandleBar() {
+		blackRect.fillAmount += Time.deltaTime / 100 ;
+	}
+}
